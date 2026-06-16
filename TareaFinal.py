9bc472561b5a67207bfb8732580a252bb588ea70
ceyuu <co.echeverriar@duocuc.cl>
2026-06-16 19:06:32 -0400

    Ejercicio terminado

--- TareaFinal.py
+++ TareaFinal.py
@@ -33,7 +33,8 @@ def validar_nombre_solicitante(valor:str):
     return False
 
 def validar_tipo_sala(valor):
-    if valor in ('P','M','G'):
+    letra = valor[0]
+    if letra in ('P','M','G'):
         return True
     return False
 
@@ -54,8 +55,6 @@ def validar_horas_reserva(valor):
         return False
     except:
         return False
-    
-# --------------
 
 def buscar_reserva(reservas,codigo_reserva):
     for posicion in range(len(reservas)):
@@ -63,9 +62,10 @@ def buscar_reserva(reservas,codigo_reserva):
             return posicion
         return -1
 
-def agregar_reserva(reservas):
+# SOLICITUD Y VALIDACION DE LOS DATOS ANTES DE LA CREACION DEL DICCIONARIO
+def agregar_reserva(reservas:list):
     while True:
-        codigo_reserva = input("Ingrese el código de reserva: ")
+        codigo_reserva = input("Ingrese un código para su reserva (debe empezar con la letra 'R' y debe contener 7 caracteres): ")
         if not validar_codigo_reserva:
             print("Error, código inválido")
             continue
@@ -73,8 +73,92 @@ def agregar_reserva(reservas):
             print("Error, el código ya fue registrado")
             continue
         break
-    
-# 3.    CONDICION DE ACTUALIZACION
 
-def actualizar_confirmaciones(reservas:list):
-    pass
+    while True:
+        nombre_solicitante = input("Ingrese el nombre del solicitante: ").title().strip()
+        if validar_nombre_solicitante(nombre_solicitante):
+            break
+        print("Error, nombre inválido")
+
+    while True:
+        tipo_sala = input("Ingrese el tipo de sala a reservar (Pequeña, Mediana o Grande): ").title().strip()
+        if validar_tipo_sala(tipo_sala):
+            break
+        print("Error, sala inválida")
+
+    while True:
+        cantidad_personas = input("Ingrese cantidad de personas que ocuparán el servicio: ")
+        if validar_cantidad_personas(cantidad_personas):
+            break
+        print("Error, cantidad inválida")
+
+    while True:
+        horas_reserva = input("Ingrese horas a reservas: ")
+        if validar_horas_reserva(horas_reserva):
+            break
+        print("Error, cantidad de horas inválidas")
+
+    # CONSTRUCCION DEL DICCIONARIO
+    reserva = {
+        "codigo_reserva" : codigo_reserva,
+        "nombre_solicitante" : nombre_solicitante,
+        "tipo_sala" : tipo_sala,
+        "cantidad_personas" : cantidad_personas,
+        "horas_reserva" : horas_reserva,
+        "estado" : False
+    }
+    # AGREGAR A LA LISTA
+    reservas.append(reserva)
+    print("Reserva registrada excitosamente")
+
+# BUSCAR RESERVA    -   AGREGARLA AL MENU, NO NECESITA BLOQUE FUNCION
+    
+# ELIMINAR RESERVA  -   PEDIR EL CODIGO DE RESERVA A ELIMINAR EN EL MENU
+def eliminar_reserva(reservas,codigo_reserva):
+    posicion =  buscar_reserva(reservas, codigo_reserva)
+    if posicion == -1:
+        print("Reserva no encontrada")
+    else:
+        reservas.pop(posicion)
+        print("Reserva eliminada exitosamente")
+
+# ACTUALIZAR CONFIRMACIONES
+def actualizar_confirmaciones(reservas):
+    for reserva in reservas:
+        if reserva["cantidad_personas"] >= 10:
+            reserva["estado"] = True
+        else:
+            reserva["estado"] == False
+    print("Estado actualizado correctamente")
+
+def mostrar_reservas(reservas:list):
+    for index, reserva in enumerate(reservas):
+        print(f"Reserva {index}: {reserva}")
+
+while True:
+    mostrar_menu()
+    opc = leer_opcion()
+    if opc==1:
+        print("Registrar Reserva")
+        agregar_reserva(reservas)
+    elif opc==2:
+        print("Buscar Reserva")
+        codigo = input("Ingrese el código de la reserva que busca: ").title().strip()
+        busqueda = buscar_reserva(reservas,codigo)
+        if busqueda == -1:
+            print("La reserva no existe")
+        else:
+            print("Reserva encontrada")
+    elif opc==3:
+        print("Eliminar Reserva")
+        codigo = input("Ingrese el código de la reserva que desea eliminar: ")
+        eliminar = eliminar_reserva(reservas,codigo)
+    elif opc==4:
+        print("Actualizar Confirmaciones")
+        actualizar_confirmaciones(reservas)
+    elif opc==5:
+        print("Mostrar Reservas")
+        mostrar_reservas(reservas)
+    elif opc==6:
+        print("Gracias por usar nuestro servicio, adios! (˶ˆᗜˆ˵)")
+        break
